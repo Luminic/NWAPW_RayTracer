e3ae095d0e86a17f738f63a3aa789d6507e1f9b3
Louis Zhang <louiszhangy@gmail.com>
2020-07-22 13:38:38 -0700

    Minor change to vertex.cpp
    My compiler wouldn't work unless I specified a message for the assert

--- src/rendering/objects/Vertex.cpp
+++ src/rendering/objects/Vertex.cpp
@@ -9,8 +9,8 @@ Vertex::Vertex(glm::vec4 position, glm::vec4 normal, glm::vec2 tex_coords) :
 {}
 
 void Vertex::as_byte_array(unsigned char byte_array[48]) {
-    static_assert(sizeof(glm::vec4) == 16);
-    static_assert(sizeof(glm::vec2) == 8);
+    static_assert(sizeof(glm::vec4) == 16, "Vertex overflow");
+    static_assert(sizeof(glm::vec2) == 8, "Vertex overflow");
 
     unsigned char const* tmp = reinterpret_cast<unsigned char const*>(&position);
     std::copy(tmp, tmp+16, byte_array);
