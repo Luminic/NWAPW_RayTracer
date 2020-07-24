f12e1c67498f1d6f1c034cc86a6d66b28793e5b5
Bruce Cosgrove <1901.brc@gmail.com>
2020-07-24 09:37:09 -0700

    Refactored ModelLoader3D and fixed copy paste bug.

--- src/rendering/ModelLoader3D.cpp
+++ src/rendering/ModelLoader3D.cpp
@@ -9,9 +9,6 @@
 #include "objects/Material.hpp"
 #include "objects/DynamicMesh.hpp"
 
-constexpr size_t MAX_VERTICES = 24; //  1M
-constexpr size_t MAX_INDICES = 12;  // 10M
-
 Node* ModelLoader3D::load_model(const char* file_path) {
     const aiScene* scene = aiImportFile(file_path, aiProcess_JoinIdenticalVertices | aiProcess_Triangulate);
 
@@ -29,21 +26,8 @@ Node* ModelLoader3D::load_model(const char* file_path) {
     for (unsigned int i = 0; i < scene->mNumMeshes; ++i) {
         aiMesh* mesh = scene->mMeshes[i];
 
-        if (mesh->mNumVertices > MAX_VERTICES) {
-            qDebug() << "Vertex count:" << mesh->mNumVertices << "exceeds maximum number of vertices" << MAX_VERTICES << "in model:" << file_path;
-            aiReleaseImport(scene);
-            return nullptr;
-        } else {
-            qDebug() << "Mesh" << i << "has" << mesh->mNumVertices << "vertices.";
-        }
-
-        if (mesh->mNumFaces > MAX_INDICES) {
-            qDebug() << "Index count:" << mesh->mNumFaces << "exceeds maximum number of indices:" << MAX_INDICES << "in model:" << file_path;
-            aiReleaseImport(scene);
-            return nullptr;
-        } else {
-            qDebug() << "Mesh" << i << "has" << mesh->mNumFaces << "indices.";
-        }
+        qDebug() << "Mesh" << i << "has" << mesh->mNumVertices << "vertices.";
+        qDebug() << "Mesh" << i << "has" << mesh->mNumFaces << "indices.";
 
         // Get material info
         aiMaterial* meshMaterial = scene->mMaterials[mesh->mMaterialIndex];
@@ -53,15 +37,14 @@ Node* ModelLoader3D::load_model(const char* file_path) {
         aiGetMaterialColor(meshMaterial, AI_MATKEY_COLOR_SPECULAR, &specular);
         aiGetMaterialColor(meshMaterial, AI_MATKEY_COLOR_DIFFUSE, &diffuse);
         aiGetMaterialColor(meshMaterial, AI_MATKEY_COLOR_AMBIENT, &ambient);
-        aiGetMaterialFloat(meshMaterial, AI_MATKEY_COLOR_SPECULAR, &shininess);
+        aiGetMaterialFloat(meshMaterial, AI_MATKEY_SHININESS, &shininess);
 
         Material material(glm::vec4(specular.r, specular.g, specular.b, shininess),
                           glm::vec4(diffuse.r, diffuse.g, diffuse.b, diffuse.a),
                           glm::vec4(ambient.r, ambient.g, ambient.b, ambient.a), this);
 
         // Get vertices
-        unsigned int vertexCount = 0;
-        std::vector<Vertex> vertices(MAX_VERTICES); // mesh->mNumVertices
+        std::vector<Vertex> vertices(mesh->mNumVertices);
         for (unsigned int j = 0; j < mesh->mNumVertices; ++j) {
             aiVector3D mesh_position = mesh->mVertices[j];
 
@@ -82,22 +65,21 @@ Node* ModelLoader3D::load_model(const char* file_path) {
                 tex_coords = glm::vec2(mesh_tex_coords.x, mesh_tex_coords.y);
             }
 
-            vertices[vertexCount++] = Vertex(position, normal, tex_coords);
+            vertices.push_back(Vertex(position, normal, tex_coords));
         }
 
         qDebug() << "Mesh" << i << (mesh->HasNormals() ? "has" : "doesn't have") << "normals.";
         qDebug() << "Mesh" << i << (mesh->HasTextureCoords(0) ? "has" : "doesn't have") << "texture coordinates.";
 
         // Get indices
-        unsigned int indexCount = 0;
-        std::vector<Index> indices(MAX_INDICES); // mesh->mNumFaces * 3 /* 3 indices per triangle */
+        std::vector<Index> indices(mesh->mNumFaces * 3 /* 3 indices per triangle */);
         for (unsigned int i = 0; i < mesh->mNumFaces; ++i) {
             assert(mesh->mFaces->mNumIndices == 3);
 
             // could be a for loop but whatever
-            indices[indexCount++] = mesh->mFaces->mIndices[0];
-            indices[indexCount++] = mesh->mFaces->mIndices[1];
-            indices[indexCount++] = mesh->mFaces->mIndices[2];
+            indices.push_back(mesh->mFaces->mIndices[0]);
+            indices.push_back(mesh->mFaces->mIndices[1]);
+            indices.push_back(mesh->mFaces->mIndices[2]);
         }
 
         // Form DynamicMesh
