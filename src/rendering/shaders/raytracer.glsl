530452c0f401bbe2073430cef4f28e7e82ce1c1f
Luminic <lukas.dot.py@gmail.com>
2020-08-05 14:10:54 -0700

    increase epsilon for triangle intersection

--- src/rendering/shaders/raytracer.glsl
+++ src/rendering/shaders/raytracer.glsl
@@ -166,7 +166,7 @@ uint rand(uint prev_rand) {
 
 // Ray-Triangle Intersection
 
-#define EPSILON 0.000001f
+#define EPSILON 0.00001f
 #define NEAR_PLANE 0.1f
 #define FAR_PLANE 100.0f
 
@@ -195,7 +195,7 @@ float ray_plane_int(vec3 ray_origin, vec3 ray_dir, vec3 plane_point, vec3 plane_
 
     return numer/denom;
 }
-
+#define TRIANGLE_INTERSECTION_EPSILON 0.00001f
 vec4 get_barycentric_coordinates(vec3 point, vec3 tri0, vec3 tri1, vec3 tri2) {
     /*
     Returns the barycentric coordinates of point if the point is in the triangle
@@ -204,11 +204,11 @@ vec4 get_barycentric_coordinates(vec3 point, vec3 tri0, vec3 tri1, vec3 tri2) {
     float double_area_tri = length(cross(tri1-tri0, tri2-tri0));
 
     float area0 = length(cross(tri1-point, tri2-point)) / double_area_tri;
-    if (area0 > 1+EPSILON) return vec4(0, 0, 0, -1);
+    if (area0 > 1+TRIANGLE_INTERSECTION_EPSILON) return vec4(0, 0, 0, -1);
     float area1 = length(cross(tri0-point, tri2-point)) / double_area_tri;
-    if (area0+area1 > 1+EPSILON) return vec4(0, 0, 0, -1);
+    if (area0+area1 > 1+TRIANGLE_INTERSECTION_EPSILON) return vec4(0, 0, 0, -1);
     float area2 = length(cross(tri0-point, tri1-point)) / double_area_tri;
-    if (area0+area1+area2 > 1+EPSILON) return vec4(0, 0, 0, -1);
+    if (area0+area1+area2 > 1+TRIANGLE_INTERSECTION_EPSILON) return vec4(0, 0, 0, -1);
 
     return vec4(area0, area1, area2, 1);
 }
